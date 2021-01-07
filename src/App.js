7213391d55c95f458be11fc32cc99b6920ad6ac0
Bisvarup Mukherjee <bisvarup.m@flipkart.com>
2021-01-07 23:58:36 +0530

    feat: updated styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
+import { Button, Layout, Typography, Divider } from "antd";
+const { Title } = Typography;
+const { Header, Footer, Sider, Content } = Layout;
+
 import W_TH from "./WithWebWorker";
 import WO_TH from "./WithoutWebWorker";
 
@@ -10,26 +14,53 @@ class App extends React.PureComponent {
 
   render() {
     return (
-      <>
-        <button
-          onClick={() => {
-            this.setState({ UI: "UN_THREADED" });
-          }}
-        >
-          Without WebWorker
-        </button>
-        <button
-          onClick={() => {
-            this.setState({ UI: "THREADED" });
-          }}
-        >
-          With WebWorker
-        </button>
-        <div>
-          {this.state.UI === "UN_THREADED" ? <WO_TH /> : null}
-          {this.state.UI === "THREADED" ? <W_TH /> : null}
-        </div>
-      </>
+      <Layout
+        style={{
+          display: "flex",
+          height: "100vh",
+          minHeight: "100vh",
+        }}
+      >
+        <Header>
+          <Title style={{ color: "#fff" }}>Web Workers 😍🔥</Title>
+        </Header>
+        <Content style={{ flex: 1, overflow: "scroll", padding: "1rem 3rem" }}>
+          <div
+            style={{
+              position: "sticky",
+              background: "#fff",
+              padding: 12,
+              top: 0,
+            }}
+          >
+            <Button
+              type='primary'
+              onClick={() => {
+                this.setState({ UI: "THREADED" });
+              }}
+            >
+              With WebWorker
+            </Button>
+            &nbsp;&nbsp;
+            <Button
+              onClick={() => {
+                this.setState({ UI: "UN_THREADED" });
+              }}
+            >
+              Without WebWorker
+            </Button>
+          </div>
+          <div style={{ display: "flex", flex: 1, overflow: "scroll" }}>
+            {this.state.UI === "UN_THREADED" ? <WO_TH /> : null}
+            {this.state.UI === "THREADED" ? <W_TH /> : null}
+          </div>
+        </Content>
+        <Footer>
+          <a href='https://github.com/bisho1995/react-web-worker-playground'>
+            GitHub
+          </a>
+        </Footer>
+      </Layout>
     );
   }
 }
